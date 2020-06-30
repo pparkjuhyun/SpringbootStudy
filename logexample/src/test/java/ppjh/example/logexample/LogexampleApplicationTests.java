f9600b8aa329742eacf8f2aa51851f283e6df932
juhyunpark <qkrwn0429@gmail.com>
2020-06-30 23:19:53 +0900

    200630 log example start

---
+++ logexample/src/test/java/ppjh/example/logexample/LogexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ppjh.example.logexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
