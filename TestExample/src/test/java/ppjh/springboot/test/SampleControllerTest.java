db15f9c7c691f7a52af727e5626c46716df6e0ac
juhyunpark <qkrwn0429@gmail.com>
2020-07-01 21:25:00 +0900

    200701 restTemplate을 이용한 테스트

--- TestExample/src/test/java/ppjh/springboot/test/SampleControllerTest.java
+++ TestExample/src/test/java/ppjh/springboot/test/SampleControllerTest.java
@@ -5,9 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,20 +19,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
+
+    @MockBean
+    SampleService mockSampleService;
 
     @Test
     public void hello() {
         try {
-            mockMvc.perform(get("/hello"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().string("hello juhyun"))
-                    .andDo(print());
+            when(mockSampleService.getName()).thenReturn("juhyun");
+
+            String result = testRestTemplate.getForObject("/hello", String.class);
+            assertThat(result);
         } catch (Exception e) {
             e.printStackTrace();
         }
