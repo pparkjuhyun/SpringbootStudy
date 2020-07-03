49d26bb1450e627f8ad4dabceb5331c4e3dffa72
juhyunpark <qkrwn0429@gmail.com>
2020-07-03 22:59:07 +0900

    200703 httpMessageConverter

--- MVCExample/src/main/java/ppjh/sb/example/mvc/user/UserController.java
+++ MVCExample/src/main/java/ppjh/sb/example/mvc/user/UserController.java
@@ -1,13 +1,22 @@
 package ppjh.sb.example.mvc.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public @ResponseBody String hello() {   //RestController일 때, ResponseBody는 생략 가능
         return "hello";
+    }
+
+//    @PostMapping("/user")
+//    public @ResponseBody User create(@RequestBody User user) {
+//        return null;
+//    }
+
+    @PostMapping("/user/create")
+    public User create(@RequestBody User user) {
+        return user;
     }
 }
