060baf297c9b79c1ed437b1010c953b66b1dfea3
juhyunpark <qkrwn0429@gmail.com>
2020-07-05 16:41:49 +0900

    200705 static resource

--- MVCExample/src/main/java/ppjh/sb/example/mvc/config/WebConfig.java
+++ MVCExample/src/main/java/ppjh/sb/example/mvc/config/WebConfig.java
@@ -1,7 +1,7 @@
 package ppjh.sb.example.mvc.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -10,5 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
 }
