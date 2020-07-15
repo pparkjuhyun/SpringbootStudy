913573535236f7cf21ecf12ff1a2f01ac605e8bf
juhyunpark <qkrwn0429@gmail.com>
2020-07-15 23:53:29 +0900

    200715 HATEOAS

---
+++ HATEOASExample/src/main/java/ppjh/springboot/hateoas/SampleController.java
@@ -0,0 +1,23 @@
+package ppjh.springboot.hateoas;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public EntityModel<Hello> hello() {
+        Hello hello = new Hello();
+        hello.setPrefix("Hey,");
+        hello.setName("Juhyun");
+
+        EntityModel<Hello> helloEntityModel = new EntityModel<>(hello);
+        helloEntityModel.add(linkTo(methodOn(SampleController.class).hello()).withSelfRel());
+        return helloEntityModel;
+    }
+}
