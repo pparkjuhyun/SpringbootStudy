cc38a2278b6fc922296634fbf4f2aeb331340362
juhyunpark <qkrwn0429@gmail.com>
2020-07-17 15:37:33 +0900

    200717 inmemoryDB(H2)

---
+++ InMemoryDB/src/main/java/ppjh/springboot/inmemorydb/InMemoryDbApplication.java
@@ -0,0 +1,13 @@
+package ppjh.springboot.inmemorydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InMemoryDbApplication.class, args);
+    }
+
+}
