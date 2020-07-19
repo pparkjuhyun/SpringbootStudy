9cb49529a42a8203d383a9b4bce1078f081f6f53
juhyunpark <qkrwn0429@gmail.com>
2020-07-19 22:24:46 +0900

    2007119 database initialize

--- JPAExample/src/main/java/ppjh/springboot/jpa/account/AccountRepository.java
+++ JPAExample/src/main/java/ppjh/springboot/jpa/account/AccountRepository.java
@@ -2,9 +2,11 @@ package ppjh.springboot.jpa.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
 //    @Query(nativeQuery = true, value = "select * from account where = '{0}'")
