3c32789154916ea9c763a0d4f5b55f0cd9e318cd
juhyunpark <qkrwn0429@gmail.com>
2020-07-23 16:11:34 +0900

    200723 RestTemplate Customizing

--- RestTemplateExample/src/main/java/ppjh/springboot/resttemplate/RestRunner.java
+++ RestTemplateExample/src/main/java/ppjh/springboot/resttemplate/RestRunner.java
@@ -21,7 +21,9 @@ public class RestRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 //        RestTemplate restTemplate = restTemplateBuilder.build();
-        WebClient webClient = builder.build();
+        WebClient webClient = builder
+//                .baseUrl("http://localhost:8080")
+                .build();
 
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
@@ -34,7 +36,7 @@ public class RestRunner implements ApplicationRunner {
 //        String worldResult = restTemplate.getForObject("http://localhost:8080/hello", String.class);
 //        System.out.println(worldResult);
 
-        Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello")
+        Mono<String> helloMono = webClient.get().uri("/hello")
                 .retrieve()
                 .bodyToMono(String.class);
 
@@ -49,7 +51,7 @@ public class RestRunner implements ApplicationRunner {
             stopWatch.start();
         });
 
-        Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world")
+        Mono<String> worldMono = webClient.get().uri("/world")
                 .retrieve()
                 .bodyToMono(String.class);
 
--- RestTemplateExample/src/main/java/ppjh/springboot/resttemplate/RestTemplateExampleApplication.java
+++ RestTemplateExample/src/main/java/ppjh/springboot/resttemplate/RestTemplateExampleApplication.java
@@ -2,6 +2,12 @@ package ppjh.springboot.resttemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class RestTemplateExampleApplication {
@@ -10,4 +16,25 @@ public class RestTemplateExampleApplication {
         SpringApplication.run(RestTemplateExampleApplication.class, args);
     }
 
+    //Customizer 대신 WebClientBuilder를 새로 등록해도 됨
+    @Bean
+    public WebClientCustomizer webClientCustomizer() {
+        return new WebClientCustomizer() {
+            @Override
+            public void customize(WebClient.Builder webClientBuilder) {
+                webClientBuilder.baseUrl("http://localhost:8080");
+            }
+        };
+    }
+
+    //Apache HttpClient를 사용하도록 설정하는 방법
+    @Bean
+    public RestTemplateCustomizer restTemplateCustomizer() {
+        return new RestTemplateCustomizer() {
+            @Override
+            public void customize(RestTemplate restTemplate) {
+                restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+            }
+        };
+    }
 }
