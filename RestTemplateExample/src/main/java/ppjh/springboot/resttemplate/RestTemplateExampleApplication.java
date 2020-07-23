dde61de68e8f65d4d52a1d8911befb6dbce945c6
juhyunpark <qkrwn0429@gmail.com>
2020-07-23 16:02:25 +0900

    200723 RestTemplate, WebClient

---
+++ RestTemplateExample/src/main/java/ppjh/springboot/resttemplate/RestTemplateExampleApplication.java
@@ -0,0 +1,13 @@
+package ppjh.springboot.resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateExampleApplication.class, args);
+    }
+
+}
---
+++ RestTemplateExample/src/test/java/ppjh/springboot/resttemplate/RestTemplateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ppjh.springboot.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
