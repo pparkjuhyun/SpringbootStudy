334f0ac620bcc53c1abfa4b0602577e8d882f42a
juhyunpark <qkrwn0429@gmail.com>
2020-07-25 13:20:43 +0900

    200724 Actuator

---
+++ ActuatorExample/src/test/java/ppjh/springboot/actuator/ActuatorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ppjh.springboot.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
