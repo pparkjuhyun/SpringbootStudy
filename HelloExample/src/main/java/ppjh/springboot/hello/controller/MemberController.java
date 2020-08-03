fdce41a1afd1c0334dd54ccdccec2cf80cf00bee
juhyunpark <qkrwn0429@gmail.com>
2020-08-03 17:31:34 +0900

    200803 mapping, template

--- HelloExample/src/main/java/ppjh/springboot/hello/controller/MemberController.java
+++ HelloExample/src/main/java/ppjh/springboot/hello/controller/MemberController.java
@@ -2,10 +2,13 @@ package ppjh.springboot.hello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ppjh.springboot.hello.domain.Member;
 import ppjh.springboot.hello.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -38,5 +41,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
