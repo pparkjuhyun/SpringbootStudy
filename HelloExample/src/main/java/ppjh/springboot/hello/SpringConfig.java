485e00e59a41a149907efa6bbe7eca23827d5648
juhyunpark <qkrwn0429@gmail.com>
2020-08-03 22:10:31 +0900

    200803 classic jdbc implementation

--- HelloExample/src/main/java/ppjh/springboot/hello/SpringConfig.java
+++ HelloExample/src/main/java/ppjh/springboot/hello/SpringConfig.java
@@ -1,13 +1,24 @@
 package ppjh.springboot.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ppjh.springboot.hello.repository.JdbcMemberRepository;
 import ppjh.springboot.hello.repository.MemberRepository;
 import ppjh.springboot.hello.repository.MemoryMemberRepository;
 import ppjh.springboot.hello.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();  //interface 구현을 통한 간단한 DB교체 예시
+        return new JdbcMemberRepository(dataSource);
     }
 }
